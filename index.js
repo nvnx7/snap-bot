650cc71ca9c68d1dc2892dac9d0df26e1dd019fa
Naveen <thenvnsahu@gmail.com>
2020-05-24 23:34:04 +0530

    wrote basic server side script

--- index.js
+++ index.js
@@ -1,16 +1,57 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
 const Twit = require("twit");
 
-const appUserName = "@snapbot";
+const typeMap = {
+  ".ico": "image/x-icon",
+  ".html": "text/html",
+  ".js": "text/javascript",
+  ".json": "application/json",
+  ".css": "text/css",
+  ".png": "image/png",
+  ".jpg": "image/jpeg",
+  ".wav": "audio/wav",
+  ".mp3": "audio/mpeg",
+  ".svg": "image/svg+xml",
+  ".pdf": "application/pdf",
+  ".doc": "application/msword",
+};
 
-const T = new Twit({
-  consumer_key: "",
-  consumer_secret: "",
-  access_token: "",
-  access_token_secret: "",
-  timeout_ms: 60 * 1000,
-  strictSSL: true,
-});
+const baseDir = "./dist";
 
-const stream = T.stream("statuses/filter", { track: appUserName });
+function handleRequests(req, res) {
+  console.log(`${req.method} ${req.url}`);
 
-stream.on("tweet", event);
+  const parsedUrl = url.parse(req.url);
+  let filePath = baseDir + parsedUrl.pathname;
+  if (filePath == `${baseDir}/`) filePath = `${baseDir}/index.html`;
+
+  const extname = path.extname(filePath);
+  const contentType = typeMap[extname];
+
+  fs.exists(filePath, (exist) => {
+    if (!exist) {
+      res.statusCode = 404;
+      res.end(`File ${fileName} not found!`);
+      return;
+    }
+
+    fs.readFile(filePath, function (err, data) {
+      if (err) {
+        if (err) {
+          res.statusCode = 500;
+          res.end(`Error getting file: ${err}.`);
+        }
+      } else {
+        res.writeHead(200, { "Content-Type": contentType });
+        res.end(data);
+      }
+    });
+  });
+}
+
+http.createServer(handleRequests).listen(8080);
+
+console.log("Server started");
