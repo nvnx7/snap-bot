57e589073754d6eac3f0c0f57b2d110fb8818646
Naveen <thenvnsahu@gmail.com>
2020-05-29 03:26:32 +0530

    added function to extract relevant tweet fields

--- controller/tweet.js
+++ controller/tweet.js
@@ -7,4 +7,25 @@ function requestTweet(tweetId, callback) {
   T.get("statuses/show/:id", { id: tweetId }, callback);
 }
 
-module.exports = requestTweet;
+function extractTweet(data) {
+  const tweet = {};
+  tweet.id = data.id_str;
+  tweet.author = data.user.name;
+  tweet.username = data.user.screen_name;
+  tweet.profilePic = data.user.profile_image_url;
+  tweet.verified = data.user.verified;
+  tweet.tweetText = data.text;
+  tweet.likeCount = data.favorite_count;
+  tweet.createdAt = data.created_at; // UTC Time
+  tweet.media = data.entities.media;
+  tweet.tokens = [
+    ...data.entities.hashtags,
+    ...data.entities.user_mentions,
+    ...data.entities.symbols,
+    ...data.entities.urls,
+  ];
+
+  return tweet;
+}
+
+module.exports = { requestTweet, extractTweet };
