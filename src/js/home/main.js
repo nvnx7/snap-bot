be66084e782d82873c96c5d6fba9de201c022d6e
Naveen <thenvnsahu@gmail.com>
2020-05-29 03:23:36 +0530

    added logic to handle error in request

--- src/js/home/main.js
+++ src/js/home/main.js
@@ -1,19 +1,41 @@
-document.getElementById("tweet-search-btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  const url = document.getElementById("tweet-link").value;
+document.addEventListener("DOMContentLoaded", () => {
+  document.getElementById("tweet-search-btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    showLoading(true);
+    // TODO Check url validity
+    const url = document.getElementById("tweet-link").value;
 
-  const data = new URLSearchParams();
-  data.append("tweetUrl", url);
+    const data = new URLSearchParams();
+    data.append("tweetUrl", url);
 
-  fetch("/tweet", {
-    method: "POST",
-    body: data,
-  })
-    .then((response) => {
-      return response.text();
+    fetch("/tweet", {
+      method: "POST",
+      body: data,
     })
-    .then((data) => {
-      console.log(`Data response: ${data}`);
-    })
-    .catch((err) => {});
+      .then((response) => {
+        if (!response.ok) {
+          const err = Error();
+          err.name = "404";
+          err.message = response.statusText;
+          throw err;
+        }
+        return response.json();
+      })
+      .then((tweetData) => {
+        console.log(`Data response: ${JSON.stringify(tweetData)}`);
+        window.sessionStorage.setItem(tweetData.id, JSON.stringify(tweetData));
+        window.location.href = `/tweet/${tweetData.id}`;
+      })
+      .catch((err) => {
+        showLoading(false);
+        console.log(`Error ${err}`);
+
+        if (err.name == "404") showErrorMessage(err.message);
+        else showErrorMessage("Network error!");
+      });
+  });
+
+  document.getElementById("tweet-link").addEventListener("focus", () => {
+    hideErrorMessage();
+  });
 });
