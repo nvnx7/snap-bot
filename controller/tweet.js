567dd21f5259bd9e2fd6fdae7ecd2bc35ac50478
Naveen <thenvnsahu@gmail.com>
2020-06-01 23:47:35 +0530

    auto replies are generated now to suitable mention

--- controller/tweet.js
+++ controller/tweet.js
@@ -9,11 +9,28 @@ function startTrackingMentions() {
   const stream = T.stream("statuses/filter", { track: `${username} snap` });
   stream.on("tweet", (tweet) => {
     console.log(`Tweet mention detected: ${JSON.stringify(tweet)}`);
+    generateReply(tweet);
   });
 }
 
 function requestTweet(tweetId, callback) {
   T.get("statuses/show/:id", { id: tweetId }, callback);
+}
+
+function generateReply(tweet) {
+  const statusId = tweet.id_str;
+  const inReplyToStatusId = tweet.in_reply_to_status_id_str;
+  const inReplyToUsername = tweet.in_reply_to_screen_name;
+  const replyText = `@${inReplyToUsername} Go to http://localhost:8080/tweet/${inReplyToStatusId}`;
+
+  T.post(
+    "statuses/update",
+    { status: replyText, in_reply_to_status_id: statusId },
+    (err, reply) => {
+      if (err) console.log(`Error replying to request: ${err}`);
+      else console.log(`Successfully replied: ${JSON.stringify(reply)}`);
+    }
+  );
 }
 
 function extractTweet(data) {
