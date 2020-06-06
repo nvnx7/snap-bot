508933df13e3384498e25fea68b8132e65347d6d
Naveen <thenvnsahu@gmail.com>
2020-06-06 23:19:24 +0530

    fixed wrong x canvas coord calc. due to scrollbar

--- src/js/tweet/captureTweet.js
+++ src/js/tweet/captureTweet.js
@@ -50,20 +50,13 @@ function composeOptions(theme) {
   options.allowTaint = true;
   options.removeContainer = true;
   options.backgroundColor = "red";
-  // options.scrollX = -9;
 
-  // options.backgroundColor = theme === "dark" ? "#000000" : "#ffffff";
+  if (scrollbarYVisible()) {
+    options.x =
+      tweetWrapper.getBoundingClientRect().left + getScrollbarWidth() / 2;
+  }
 
-  const viewportW = window.innerWidth;
-  const viewportH = window.innerHeight;
-  const headerH = document.getElementById("navbar").clientHeight;
-  const wrapper = document.getElementById("tweet-wrapper");
-
-  if (wrapper.clientWidth - viewportW > 0)
-    options.scrollX = -(wrapper.clientWidth - viewportW);
-
-  if (wrapper.clientHeight + headerH - viewportH > 0)
-    options.scrollY = wrapper.clientHeight + headerH - viewportH;
+  options.backgroundColor = theme === "dark" ? "#000000" : "#ffffff";
 
   return options;
 }
@@ -132,4 +125,32 @@ function createErrorMessage() {
   const elem = document.createElement("DIV");
   elem.id = "error-message";
   return elem;
+}
+
+function getScrollbarWidth() {
+  // Creating invisible container
+  const outer = document.createElement("div");
+  outer.style.visibility = "hidden";
+  outer.style.overflow = "scroll"; // forcing scrollbar to appear
+  outer.style.msOverflowStyle = "scrollbar"; // needed for WinJS apps
+  document.body.appendChild(outer);
+
+  // Creating inner element and placing it in the container
+  const inner = document.createElement("div");
+  outer.appendChild(inner);
+
+  // Calculating difference between container's full width and the child width
+  const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+
+  // Removing temporary elements from the DOM
+  outer.parentNode.removeChild(outer);
+
+  return scrollbarWidth;
+}
+
+function scrollbarYVisible() {
+  return (
+    document.documentElement.scrollHeight >
+    document.documentElement.clientHeight
+  );
 }
