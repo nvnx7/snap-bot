afb85da5de8a66a0d4f817fe1b41d7ca46343316
Naveen <thenvnsahu@gmail.com>
2020-06-06 17:47:37 +0530

    large numbers are abbreviated now

--- src/js/tweet/tweetPreview.js
+++ src/js/tweet/tweetPreview.js
@@ -81,9 +81,18 @@ function setTweetLiked(isLiked) {
   tweetWrapper.querySelector("#like-count").style.color = textColor;
 }
 
+function abbreviateNumber(n) {
+  if (n < 1e3) return n;
+  if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + "K";
+  if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + "M";
+  if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + "B";
+}
+
 function setTweetFooter(likeCount, utcString) {
-  tweetWrapper.querySelector("#like-count").innerHTML = likeCount;
-  tweetWrapper.querySelector("#tweet-time").innerHTML = getLocaleDateString(
+  tweetWrapper.querySelector("#like-count").textContent = abbreviateNumber(
+    likeCount
+  );
+  tweetWrapper.querySelector("#tweet-time").textContent = getLocaleDateString(
     utcString
   );
 }
