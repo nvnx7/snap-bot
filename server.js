727f6ba471fe563c3200f85f467e00430e756581
Naveen <thenvnsahu@gmail.com>
2020-06-08 17:39:55 +0530

    removed test code/comments

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const favicon = require("serve-favicon");
 
 dotenv.config();
 
-const typeMap = require("./model/constants.js");
 const {
   requestTweet,
   extractTweet,
@@ -15,25 +14,6 @@ const {
 } = require("./controller/tweet.js");
 
 const app = express();
-
-const sample = {
-  author: "Wisdom Theory",
-  username: "wealth_theory",
-  verified: false,
-  profilePic:
-    "https://pbs.twimg.com/profile_images/1242963480707248128/ZCzQHDaw_normal.jpg",
-  tweetText: `Everyone you meet is fighting a #battle you know nothing about.<br /><br />Be #kind.<br /><br />Always. @random`,
-  tweetImage: "",
-  likeCount: 234,
-  timeString: "3:04 AM - May 20, 2020",
-  tokens: ["#battle", "#kind", "@random"],
-};
-
-const sample2 = {
-  author: "Wisdom Theory",
-  username: "wealth_theory",
-  verified: false,
-};
 
 const reg = /^\d+$/;
 app.use(cors());
@@ -54,23 +34,16 @@ app.get("/", (req, res) => {
 });
 
 app.post("/tweet", (req, res) => {
-  // console.log(`Req Body at server: ${JSON.stringify(req.body)}`);
   const tweetId = url.parse(req.body.tweetUrl).pathname.split("/").slice(-1)[0];
-  // console.log(`Id: ${tweetId}`);
   if (!reg.test(tweetId))
     res.status(404).type("text").send(`Invalid tweet id: ${tweetId}`);
 
   requestTweet(tweetId, (err, data, response) => {
-    // console.log(`Err: ${JSON.stringify(err)}`);
-    // console.log(`Data: ${JSON.stringify(data)}`);
-    // console.log(`Response: ${JSON.stringify(response)}`);
-
     if (err) {
       res.statusMessage = err.message;
       res.status(404).end();
     } else {
       const tweetData = extractTweet(data);
-      console.log(`Tweet Data: ${JSON.stringify(tweetData)}`);
       res.status(200).json(tweetData);
     }
   });
